7118d73105fd61a1796c9b3c73dc2043abec4f19
JIHWAN <jjjh0508@gmail.com>
2023-08-20 19:26:31 +0900

    칸 제거 추가

--- js/tetris.js
+++ js/tetris.js
@@ -88,6 +88,24 @@ function seizeBlock(){
         moving.classList.remove("moving");
         moving.classList.add("seized");
     })
+    checkMatch();
+}
+
+function checkMatch(){
+    const childNodes = playground.childNodes;
+    childNodes.forEach(child=>{
+        let matched = true;
+        child.children[0].childNodes.forEach(li=>{
+            if(!li.classList.contains("seized")){
+                matched = false;
+            }
+        })
+        if(matched){
+            child.remove();
+            prependNewLine();
+        }
+    })
+
     generateNewBlock();
 }
 
