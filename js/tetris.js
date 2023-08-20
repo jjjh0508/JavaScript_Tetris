0cdb44801cdc14cf9fe19c2507c833945d896b25
JIHWAN <jjjh0508@gmail.com>
2023-08-20 18:07:26 +0900

    블럭 움직임

--- js/tetris.js
+++ js/tetris.js
@@ -1,15 +1,112 @@
+//DOM
 const playground = document.querySelector(".playground>ul");
 
-console.log(playground);
+//Setting
+const GAME_ROWS =20;
+const GAME_COLS =10;
 
-for(let i=0;i<20;i++){
+//variables
+let score = 0;
+let duration = 500;
+let dounInterval;
+let tempMovingItem;
+
+const BLOCKS = {
+    tree : [
+        [[2,1],[0,1],[1,0],[1,1]],
+        [],
+        [],
+        [],
+    ]
+}
+
+
+const movingItem ={
+    type : "tree",
+    direction: 0,
+    top : 0,
+    left : 3,
+
+};
+
+
+init();
+
+//functions
+function init(){
+    tempMovingItem = {...movingItem };
+
+    for(let i=0;i<GAME_ROWS;i++){
+        prependNewLine();
+    }
+    renderBlocks();
+}
+
+
+
+function prependNewLine(){
     const li = document.createElement("li");
     const ul = document.createElement("ul");
-    for(let j=0;j<10;j++){
+    for(let j=0;j<GAME_COLS;j++){
         const matrix = document.createElement("li");
         ul.prepend(matrix);
     }
 
     li.prepend(ul);
     playground.prepend(li);
+};
+
+
+function renderBlocks(){
+    const { type, direction, top, left } =tempMovingItem;
+    const moveBlocks= document.querySelectorAll(".moving");
+    moveBlocks.forEach(moving=>{
+        moving.classList.remove(type,"moving")
+    })
+    BLOCKS[type][direction].forEach(block => {
+        const x = block[0]+left;
+        const y = block[1]+top;
+        const target = playground.childNodes[y] ? playground.childNodes[y].childNodes[0].childNodes[x]: null;
+        const isAvailable = checkEmpty(target);
+        if(isAvailable){
+            target.classList.add(type,"moving");
+        }else{
+            tempMovingItem = {...movingItem }
+            setTimeout(()=>{
+                renderBlocks();
+            },0)
+        }
+    });
+    movingItem.left =left;
+    movingItem.top =top;
+    movingItem.direction =direction;
 }
+function checkEmpty(target){
+    if(!target){
+        return false;
+    }
+    return true;   
+}
+
+function moveBlock(moveType,amount){
+    tempMovingItem[moveType] += amount;
+    renderBlocks();
+}
+
+
+// event handling
+document.addEventListener("keydown",e=>{
+    switch(e.keyCode){
+        case 39:
+            moveBlock("left",1);
+            break;
+        case 37:
+            moveBlock("left",-1);
+            break;
+        case 40:
+            moveBlock("top",1)
+            break;
+        default:
+            break;
+    }
+})
